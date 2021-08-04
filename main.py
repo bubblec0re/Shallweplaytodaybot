1995e64fc3347c7dac73d9ecb0251f50816c8fff
Vladimir <vova206@gmail.com>
2021-08-04 14:06:29 +0300

    added black
    Signed-off-by: Vladimir <vova206@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import telebot
 
-bot = telebot.TeleBot('***REMOVED***')
+bot = telebot.TeleBot("***REMOVED***")
 
-@bot.message_handler(commands=['help'])
+
+@bot.message_handler(commands=["help"])
 def reply_to_help(message):
-    bot.send_message(message.from_user.id, "Я ежедневно высылаю в группу опросник с вопросом, будем ли мы сегодня играть.")
+    bot.send_message(
+        message.from_user.id,
+        "Я ежедневно высылаю в группу опросник с вопросом, будем ли мы сегодня играть.",
+    )
 
-@bot.message_handler(content_types=['text'])
+
+@bot.message_handler(content_types=["text"])
 def get_text_messages(message):
     if message.text == "Тест":
         bot.send_message(message.from_user.id, "Тест сработал")
     else:
-        bot.send_message(message.from_user.id, "Другого функционала пока не предусмотрено.")
+        bot.send_message(
+            message.from_user.id, "Другого функционала пока не предусмотрено."
+        )
+
 
 bot.polling(True, 5)
