3fb0309a48067fdeb65c8ffdfb13f1e34d3e3919
Vladimir <vova206@gmail.com>
2021-08-05 16:59:56 +0300

    scheduler
    Signed-off-by: Vladimir <vova206@gmail.com>

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
 import telebot
-import json
+import json, sched, time
 
-bot = telebot.TeleBot("***REMOVED***")
 # https://api.telegram.org/bot***REMOVED***/getupdates
-
 
 try:
     f = open("settings.json")
-    bot_settings = json.load(f)
+    botSettings = json.load(f)
 finally:
-    bot_settings = {"groupID": None, "lastSentPollID": None, "autoSendPoll": False}
+    botSettings = {
+        "groupID": None,
+        "lastSentPollID": None,
+        "autoSendPoll": False,
+        "timeForPoll": 0,
+    }
+
+
+bot = telebot.TeleBot("***REMOVED***")
 
 
 @bot.message_handler(commands=["poll"])
-def poll_reply(message):
+def poll_reply(message: telebot.types.Message):
+
     save_message(message)
 
     chat_id = message.chat.id
-    reply = send_poll_reply(message)
+    reply = send_poll(chat_id)
     save_message(reply)
-    bot.pin_chat_message(chat_id, reply.message_id)
 
 
-def send_poll_reply(message: telebot.types.Message):
-    chat_id = message.chat.id
-    reply = bot.send_poll(
+def send_poll(chat_id: int):
+
+    sentPoll = bot.send_poll(
         chat_id,
         "Будем играть сегодня?",
         ["Да", "Скорее да", "Скорее нет", "Нет", "Пока не знаю"],
         False,
     )
-    return reply
+
+    botSettings.lastSentPollID = sentPoll.message_id
+    bot.pin_chat_message(chat_id, sentPoll.message_id)
+    return sentPoll
 
 
 @bot.message_handler(commands=["autopollon", "autopolloff"])
 def toggle_autopoll(message: telebot.types.Message):
-    if message.text == "/autopollon":
-        bot_settings.autoSendPoll = True
-    elif message.text == "autopolloff":
-        bot_settings.autoSendPoll = False
 
-    save_settings()
+    if message.text == "/autopollon":
+        botSettings.autoSendPoll = True
+    elif message.text == "autopolloff":
+        botSettings.autoSendPoll = False
+
+    save_settings_to_file()
 
 
 @bot.message_handler(commands=["help"])
@@ -65,7 +75,7 @@ def membership_update_handler(chat_member_updated: telebot.types.ChatMemberUpdat
         chat_info = vars(chat_member_updated.chat)
         chat_info["AmMember"] = chat_member_updated.new_chat_member.is_member
         json.dump(chat_info, f, indent=4, ensure_ascii=False)
-    bot_settings.groupID = chat_member_updated.chat.id
+    botSettings.groupID = chat_member_updated.chat.id
 
 
 def save_message(message: telebot.types.Message):
@@ -73,9 +83,12 @@ def save_message(message: telebot.types.Message):
         json.dump("/n" + message.json, f, indent=4, ensure_ascii=False)
 
 
-def save_settings():
+def save_settings_to_file():
     with open("settings.json", "w") as f:
-        json.dump(bot_settings, f)
+        json.dump(botSettings, f)
 
+
+pollScheduler = sched.scheduler(time.time, time.sleep)
+pollScheduler.enterabs(time.time() + 60, 1, send_poll, (botSettings.groupID))
 
 bot.polling(True, 2)
